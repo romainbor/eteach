10369556f822ea75df5b325df9990fac3bb94982
romainbor <bordier.romain@yahoo.fr>
2019-07-18 14:14:24 +0200

    zz

--- server/libs/db.js
+++ server/libs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbRoute = "mongodb+srv://daisho:pepevador80@cluster0-unpzv.mongodb.net/esgi"
+const dbRoute = "mongodb+srv://romain:Pepevador80@cluster0-unpzv.mongodb.net/test"
 
 mongoose.connect(dbRoute, { useNewUrlParser: true });
 const db = mongoose.connection;
