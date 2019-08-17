986777a9282613bde978f6c063977c66957afe1a
romainbor <bordier.romain@yahoo.fr>
2019-08-17 14:25:39 +0200

    14

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const mailRouter = require('./routes/mail')
 const path = require('path');
 
 
-const isDev = process.env.NODE_ENV !== 'production';
+//const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 const PORT_CHAT = 3231
 
