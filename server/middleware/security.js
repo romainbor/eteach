0aff2d7e144496f6f3a56f5f37c856f43180b274
romainbor <bordier.romain@yahoo.fr>
2019-08-22 01:13:43 +0200

    33

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -1,6 +1,6 @@
 const verifyJWTToken = require('../libs/auth').verifyToken;
 
-const access_routes = ["/login_check", "/user", "/mail/send", "/register"]
+const access_routes = ["/login_check", "/user", "/mail/send", "/security/register", ""]
 
 const verifyToken = (req, res, next) => {
     if(access_routes.indexOf(req.path) > -1) {
