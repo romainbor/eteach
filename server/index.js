83edfad78b5fdaa5d31f4dcb0d640799127562e1
romainbor <bordier.romain@yahoo.fr>
2019-08-23 14:55:37 +0200

    33

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ chat.listen(PORT_CHAT, ()=>{
 //app.use('/', express.static(path.join(__dirname, '/client/build')));
 //app.use('/', express.static(path.join(__dirname, '/client/build')));
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build')); // serve the static react app
+  app.use(express.static('../client/build')); // serve the static react app
   app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
     res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
