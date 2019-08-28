03f8cc6c20f8c9511898b3c9f66b859e201ed991
romainbor <bordier.romain@yahoo.fr>
2019-08-28 21:34:19 +0200

    114

--- server/index.js
+++ server/index.js
@@ -19,18 +19,14 @@ const app = express();
 const cors = require('cors');
 
 
-
-var chat = require('http').createServer()
-var io = module.exports.io = require('socket.io')(chat)
-
+var chat = require('https').createServer(app)
+var io = module.exports.io = require('socket.io').listen(chat)
 
 const SocketManager = require('./SocketManager')
 
 io.on('connection', SocketManager)
 
-chat.listen(PORT, ()=>{
-	console.log("Connected to port:" + PORT);
-})
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build')); // serve the static react app
