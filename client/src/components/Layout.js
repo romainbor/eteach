739594fe036336dc524bb0a9e2f61fecfa117f34
romainbor <bordier.romain@yahoo.fr>
2019-08-29 10:16:25 +0200

    128

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -4,7 +4,7 @@ import { USER_CONNECTED, LOGOUT, VERIFY_USER } from '../Events'
 import LoginForm from './LoginForm'
 import ChatContainer from './chats/ChatContainer'
 
-const socketUrl = "https://teachonline.herokuapp.com"
+const socketUrl = "/"
 export default class Layout extends React.Component {
 
 	constructor(props) {
