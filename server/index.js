5438b6e0f1740b6a2df4616af7063384f51e4793
romainbor <bordier.romain@yahoo.fr>
2019-08-29 16:53:30 +0200

    148

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 //const PORT_CHAT = 3231;
 
-
 const app = express();
 const cors = require('cors');
 const SocketManager = require('./SocketManager')
